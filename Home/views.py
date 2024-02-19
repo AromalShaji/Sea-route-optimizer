42a4a59a49080b44f4ee5bbd11b496287d279fcd
AromalShaji <92853159+AromalShaji@users.noreply.github.com>
2024-02-19 09:35:37 +0530

    191

--- Home/views.py
+++ Home/views.py
@@ -6,6 +6,7 @@ from django.views.decorators.cache import cache_control
 from .models import useradmin
 from django.http import JsonResponse
 from django.http import HttpResponse, FileResponse, HttpResponseRedirect
+from django.contrib import messages
 
 #====================================================================================
 #----------------------------------------home----------------------------------------
@@ -17,7 +18,6 @@ def home(request):
         userType=request.session['userType']
         if (useradmin.objects.filter(id=id, role=userType)).exists():
             dis = useradmin.objects.get(id=id, role=userType)
-            # return HttpResponse()
             return render(request, 'Home/index.html', {'id': id, 'userDeatils': dis, 'userType' : dis.role})    
     return render(request,'Home/index.html')
 
@@ -33,10 +33,12 @@ def signinPage(request):
             dis = useradmin.objects.get(email=useremail, password=password)
             request.session['id'] = dis.id
             request.session['userType'] = dis.role
+            messages.success(request, "Sign In Successfully")
             return redirect('home')
-    else:
-        msg = "wrong user name or password or account does not exist!!"
-        return render(request, 'signin.html', {'msg': msg})
+        else:
+            msg = "wrong user name or password or account does not exist!!"
+            messages.error(request, msg)
+            return render(request, 'signin.html')
     return render(request,'signin.html')
 
 
@@ -65,4 +67,5 @@ def addCrewPage(request):
 @cache_control(no_cache=True, must_revalidate=True, no_store=True)
 def logout(request):
     request.session.flush()
+    messages.success(request, "Logout Successfull")
     return redirect('home')
